a225191e2a9509b992f96f4e87872998d81b49e4
towednack <nico.schwingel2007@gmail.com>
2024-11-25 20:18:01 -0300

    Backgrounds com imagens, pequenos ajustes.

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ function iframeResetToIndex() {
         document.getElementById("iframeMain").contentWindow.location.href = "inicio.html"
     }
 }
+
+function changeThemeImg() {
+    var totalNumberOfImages = 2
+
+    var themeImg = document.getElementById('indexBackgroundImage'),
+    style = themeImg.currentStyle || window.getComputedStyle(themeImg, false),
+    backgroundImg = style.backgroundImage,
+    themeImgCurrentNumber = (backgroundImg.slice(4, -6).replace(/"/g, "")).substr((backgroundImg.slice(4, -6).replace(/"/g, "")).length - 1);
+
+    if (themeImgCurrentNumber != totalNumberOfImages) {
+        themeImg.style.backgroundImage = "url('media/background/dark/graffiti" + (+themeImgCurrentNumber + 1) +".png')"
+    }
+    else {
+        themeImg.style.backgroundImage = "url('media/background/dark/graffiti1.png')"
+    }
+}
